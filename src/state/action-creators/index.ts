6031e7a7fc77392c2356ed82580e64b9c7b15f33
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-18 16:14:51 +0100

    react redux

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,34 @@
+//functions dispatch action
+
+import { ActionType } from "../acttion-types"
+import {Dispatch} from "redux"
+import { Action } from "../actions/index"
+
+ export const depositMoney = (amount: number) => {
+return (dispatch: Dispatch<Action>) => {
+    dispatch({
+        type: ActionType.DEPOSIT,
+        payload:amount
+    })
+}
+}
+
+
+export const withdrawMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.WITHDRAW,
+            payload:amount
+        })
+    }
+    }
+
+
+ export const bankrupt = () => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.BANKRUPT,
+           
+        })
+    }
+    }
---
+++ src/state/store.ts
@@ -0,0 +1,10 @@
+import { legacy_createStore as createStore} from 'redux'
+import { applyMiddleware} from "redux"
+import thunk from "redux-thunk"
+import reducers from "./reducers/index"
+
+export const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(thunk)
+)
