ad76c4b1e092ceee30e9d8d92ee690d101ddd32b
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-18 14:29:54 +0100

    reducer but file messy

--- src/state/reducers/bankReducer.ts
+++ src/state/reducers/bankReducer.ts
@@ -2,7 +2,7 @@ const initialState = 0;
 
 interface DepositAction {
     type: "deposit",
-    payload? :number
+    payload :number
 }
 
 interface WithdrawlAction {
@@ -16,19 +16,19 @@ interface BankruptAction {
 
 type Action  = DepositAction | WithdrawlAction | BankruptAction
 
-const reducer = (state: number, action:Action) => {
+const reducer = (state: number = initialState, action: Action) => {
 
 
 
 switch (action.type){
     case "deposit" :
-        return state + action.payload;
-        case "withdraw" : 
-        return state -action.payload;
-        case "bankrupt" :
-            return 0;
-            default : 
-            return state
+     return state + action.payload;
+    case "withdraw" : 
+    return state - action.payload;
+    case "bankrupt" :
+    return 0;
+    default : 
+    return state
 
 
 }
